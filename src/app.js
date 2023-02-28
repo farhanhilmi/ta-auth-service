182713ff79aefee1fe2af98703b88429d1e6b278
Farhan Hilmi <farhanhilmi077@gmail.com>
2023-02-28 14:42:17 +0700

    gloud config

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import Routes from './routes/index.js';
 export default async (channel) => {
     const app = express();
     app.use(express.json());
-    app.use(cors({ origin: ['http://localhost:8000'] }));
+    app.use(cors({ origin: '*' }));
+    // app.use(cors({ origin: ['http://localhost:8000'] }));
 
     app.use(Routes(channel));
     return app;
